e642c86f7b3713163561c782abd09086c07a22c6
LZ <lizhan_mit@outlook.com>
2019-05-06 19:42:40 +0800

    Added Sword to Offer Q3.

---
+++ src/book/swordToOffer/Q3b.java
@@ -0,0 +1,34 @@
+package book.swordToOffer;
+
+public class Q3b {
+
+	public static int getDuplicate(int[] number) {
+		int length = number.length;
+		
+		if (number == null || length == 0) {
+			return -1;
+		}
+		
+		for (int i = 0; i < length; i++) {
+			if (number[i] <= 0 || number[i] >= length) {
+				return -1;
+			}
+		}
+		
+		int[] temp = new int[length];
+		
+		for (int i = 0; i < length; i++) {
+			if (number[i] == temp[number[i]]) {
+				return number[i];
+			}
+			temp[number[i]] = number[i];
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		int[] numbers = {2, 1, 5, 4, 3, 2, 6, 3};
+        System.out.println(getDuplicate(numbers));
+	}
+}
