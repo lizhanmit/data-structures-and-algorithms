a39455a7abaa875acdb01035ac18bdc297dbdf07
lizhanmit <lizhan_mit@outlook.com>
2021-12-12 11:56:49 +0800

    Added a better solution.

--- src/com/newcoder/huaweiexam/LengthOfStringLastWord.java
+++ src/com/newcoder/huaweiexam/LengthOfStringLastWord.java
@@ -12,10 +12,27 @@ public class LengthOfStringLastWord {
 		
 		System.out.println(length);
 	}
-	
+
+	// O(n) space because of an extra string array
+    // O(n) time
 	private static int lengthOfStringLastWord(String str) {
 		String[] strArr = str.split("\\s+");
 		
 		return strArr[strArr.length - 1].length();
 	}
+
+	// this one is better as O(1) space
+    // O(n) time
+    private static int lengthOfStringLastWord2(String str) {
+	    int index = -1;  // this must be -1
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            if (str.charAt(i) == ' ') {
+                index = i;
+                break;
+            }
+        }
+
+        return str.length() - 1 - index;
+    }
+
 }
